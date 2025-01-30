e418d04b9275cb792557c8a7e00bd2a4c2b38406
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2025-01-30 17:41:30 -0300

    FEAT: Add CardModule to application imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,9 @@ import { EmailService } from "./email/email.service";
 import { UserTypeModule } from './user_type/user_type.module';
 import { PictureModule } from './picture/picture.module';
 import { JobModule } from './job/job.module';
-
+import { CardModule } from './card/card.module';
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: ['.env.development.local', '.env.development'],}), CollaboratorModule, CompanyModule, UserModule, UserTypeModule, PictureModule, JobModule ],
+  imports: [ConfigModule.forRoot({ envFilePath: ['.env.development.local', '.env.development'],}), CollaboratorModule, CompanyModule, UserModule, UserTypeModule, PictureModule, JobModule, CardModule ],
   controllers: [AppController],
   providers: [AppService, EmailService],
 })
